266c53439a2bbdfe4e2f121de3925faec1703a80
Matheus Jagi <matheus.jagi@basis.com.br>
2022-02-12 00:25:28 -0300

    Criação incial do projeto

---
+++ src/main/java/br/com/jagi/aws_learn/AwsLearnApplication.java
@@ -0,0 +1,13 @@
+package br.com.jagi.aws_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jagi/aws_learn/AwsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jagi.aws_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
